91d355645e1756d9895fa693d9af62a1a3406957
Jordy <jordy.lopez2003@gmail.com>
2024-04-02 14:39:36 -0400

    test

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express();
+
+//controllers
+
+const sendFile = (req, res, next) => {
+    res.sendFile(__dirname + '/index.html');
+}
+
+const sendHtmlResponse = (req, res, next) => {
+    res.send('<h1> Welcome to my Website!!! <h1>')
+}
+
+
+const sendFileResponse = (req, res, next) => {
+    res.send('file content goes here');
+}
+
+const sendDataResponse = (req, res, next) => {
+    const data = [{ name: 'Chris' }, { name: 'Killian' }, { name: 'Willy' }];
+    res.send(data);
+}
+
+//endpoints
+app.get('/html', sendHtmlResponse);
+app.get('/data', sendDataResponse);
+app.get('/file', sendFile);
+app.get('/fileresponse', sendFileResponse);
+
+
+
+//listen
+const port = 8080;
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
